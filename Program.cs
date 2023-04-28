525f73dcfb8465f1821db2d7781523e88671a46d
Vinicius-G-R <125162352+Vinicius-G-R@users.noreply.github.com>
2023-04-28 18:32:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿    Console.Clear();
+int valorGlicemia;
+    Console.ForegroundColor = ConsoleColor.DarkMagenta; 
+    Console.WriteLine("--- Diagnóstico de Diabetes Gestacional ---");
+    Console.ResetColor();
+    Console.Write("\nGlicemia em mg/dL...: ");
+    valorGlicemia = Convert.ToInt32(Console.ReadLine());
+
+    if (valorGlicemia < 92) 
+    {
+    Console.ForegroundColor = ConsoleColor.Yellow; 
+    Console.WriteLine("\nReavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
+    Console.ResetColor();
+    }
+    
+    else if (valorGlicemia > 91 && valorGlicemia < 126)
+    {
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("\nCompatível com Diabetes Gestacional");   
+    Console.ResetColor();
+    }
+    
+    else 
+    {
+      Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("\nDiabetes Mellitus na Gravidez");
+      Console.ResetColor();
+    } 
